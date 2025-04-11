64a78b7a7b014b8f3a27a4b4a9c128f15ba1de4d
nijoe1 <nikolaos@gitcoin.co>
2025-04-11 14:53:40 +0300

    feat: added footer

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import { SideNav } from "./components/main/SideNav";
 import { useProgram } from "./providers/ProgramProvider";
 import { ProgramDropDown } from "./components/main/ProgramDropDown";
 import { Home } from "./pages/Home";
-
+import Footer from "./components/main/Footer";
 const queryClient = new QueryClient();
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
@@ -18,16 +18,21 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   const { programs } = useProgram();
 
   // If we're on a round page, find the program that contains this round
-  const program = roundId && chainId 
-    ? programs.find(p => p.rounds.some(r => r.id === roundId && r.chainId === Number(chainId)))
-    : null;
+  const program =
+    roundId && chainId
+      ? programs.find((p) =>
+          p.rounds.some(
+            (r) => r.id === roundId && r.chainId === Number(chainId)
+          )
+        )
+      : null;
 
   return (
     <div className="flex min-h-screen p-6">
-      {(programId || program) && <SideNav programId={programId || program?.projectId || ""} />}
-      <div className="flex-1">
-        {children}
-      </div>
+      {(programId || program) && (
+        <SideNav programId={programId || program?.projectId || ""} />
+      )}
+      <div className="flex-1">{children}</div>
     </div>
   );
 };
@@ -46,11 +51,10 @@ const RoundRoute = () => {
 };
 
 const ProgramRoute = () => {
-
   return (
-      <Layout>
-        <ProgramDetails/>
-      </Layout>
+    <Layout>
+      <ProgramDetails />
+    </Layout>
   );
 };
 
@@ -60,16 +64,20 @@ export const App = () => {
       <BrowserRouter>
         <ProgramProvider>
           <DonationProvider>
-            <Navbar showDivider={false} text={{ text: "Report Cards", link: "/" }}>
+            <Navbar
+              showDivider={false}
+              text={{ text: "Report Cards", link: "/" }}
+            >
               <ProgramDropDown />
             </Navbar>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/:chainId/:roundId" element={<RoundRoute />} />
-            <Route path="/program/:programId" element={<ProgramRoute />} />
-          </Routes>
-          </DonationProvider>   
-          </ProgramProvider>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/:chainId/:roundId" element={<RoundRoute />} />
+              <Route path="/program/:programId" element={<ProgramRoute />} />
+            </Routes>
+          </DonationProvider>
+        </ProgramProvider>
+        <Footer />
       </BrowserRouter>
     </QueryClientProvider>
   );
